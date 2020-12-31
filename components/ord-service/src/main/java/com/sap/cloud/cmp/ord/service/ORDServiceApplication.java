dce5a4b6268c8ed75afe4d29e3ffc89fde739cac
NickyMateev <bgdunkers@gmail.com>
2020-12-31 21:08:26 +0200

    Initial implementation

---
+++ components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/ORDServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sap.cloud.cmp.ord.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ORDServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ORDServiceApplication.class, args);
+    }
+
+}
