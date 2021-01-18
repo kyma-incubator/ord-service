b09685b6994fbc6715f2861226684e642c0d6f02
NickyMateev <bgdunkers@gmail.com>
2021-01-19 00:12:28 +0200

    Address review comments

--- components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/filter/ResponseFormatFilter.java
+++ components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/filter/ResponseFormatFilter.java
@@ -1,6 +1,5 @@
 package com.sap.cloud.cmp.ord.service.filter;
 
-import org.apache.commons.codec.binary.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -9,12 +8,13 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.IOException;
+import java.util.*;
 
 @Component
 @Order(Integer.MIN_VALUE)
 public class ResponseFormatFilter implements Filter {
 
-    private static final String ACCEPT_HEADER = "Accept";
+    private static final String ACCEPT_HEADER = "accept";
 
     @Value("${server.default_response_type}")
     private String defaultResponseType;
@@ -26,27 +26,35 @@ public class ResponseFormatFilter implements Filter {
         }
 
         @Override
-        public String getQueryString() {
-            String query = super.getQueryString();
-            String acceptHeader = super.getHeader(ACCEPT_HEADER);
-
-            boolean acceptsEverything = StringUtils.equals(acceptHeader, "*/*");
-
-            if (!acceptsEverything) {
-                return query;
+        public Enumeration<String> getHeaders(String name) {
+            if (!name.equals(ACCEPT_HEADER)) {
+                return super.getHeaders(name);
             }
 
-            if (query == null) {
-                return "$format=" + defaultResponseType;
+            final String query = super.getQueryString();
+            final Enumeration<String> headerEnumeration = super.getHeaders(ACCEPT_HEADER);
+            final List<String> headerValues = Collections.list(headerEnumeration);
+
+            boolean acceptsEverything = headerValues.size() == 0 || (headerValues.size() == 1 && headerValues.get(0).equals("*/*"));
+            boolean formatProvided = query != null && query.contains("$format");
+
+            if (acceptsEverything && !formatProvided) {
+                return Collections.enumeration(Arrays.asList("application/" + defaultResponseType));
             }
 
-            if (!query.contains("$format")) {
-                return query + "&$format=" + defaultResponseType;
-            }
+            return headerEnumeration;
+        }
 
-            return query;
+        @Override
+        public Enumeration<String> getHeaderNames() {
+            Enumeration<String> headerEnumeration = super.getHeaderNames();
+            Set<String> headerNames = new HashSet<>(Collections.list(headerEnumeration));
+            headerNames.add(ACCEPT_HEADER);
+
+            return Collections.enumeration(headerNames);
         }
     }
+
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
