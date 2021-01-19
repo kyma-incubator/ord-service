3518f74f1c3a669e84ff946041f307ce0eb30f8f
Nikolay Stoykov <nikolay.stoykov@sap.com>
2021-01-19 15:44:47 +0200

    Tenant filtering

--- components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/controller/ODataController.java
+++ components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/controller/ODataController.java
@@ -1,16 +1,21 @@
 package com.sap.cloud.cmp.ord.service.controller;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sap.olingo.jpa.processor.core.api.JPAClaimsPair;
 import com.sap.olingo.jpa.processor.core.api.JPAODataCRUDContextAccess;
+import com.sap.olingo.jpa.processor.core.api.JPAODataClaimsProvider;
 import com.sap.olingo.jpa.processor.core.api.JPAODataGetHandler;
+
 import org.apache.olingo.commons.api.ex.ODataException;
-import org.apache.olingo.server.api.debug.DefaultDebugSupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/${odata.jpa.request_mapping_path}/**")
@@ -24,7 +29,19 @@ public class ODataController {
         final JPAODataGetHandler handler = new JPAODataGetHandler(serviceContext);
         //handler.getJPAODataRequestContext().setDebugSupport(new DefaultDebugSupport()); // Use query parameter odata-debug=json to activate.
         // Activate debug support after security is in place
-
+        handler.getJPAODataRequestContext().setClaimsProvider(createClaims(request));
         handler.process(request, response);
     }
+
+    private JPAODataClaimsProvider createClaims(final HttpServletRequest req) {
+        final String auth = req.getHeader("tenant");
+        System.out.println(">>>>>>>>>" + auth);
+        final JPAODataClaimsProvider claims = new JPAODataClaimsProvider();
+        if (auth != null && !auth.isEmpty()) {
+          System.out.println(">>>>>>> " + auth);
+          final JPAClaimsPair<String> user = new JPAClaimsPair<>(auth);
+          claims.add("tenant_id", user);
+        }
+        return claims;
+      }
 }
--- components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/storage/model/PackageEntity.java
+++ components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/storage/model/PackageEntity.java
@@ -1,11 +1,29 @@
 package com.sap.cloud.cmp.ord.service.storage.model;
 
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.SecondaryTable;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.sap.olingo.jpa.metadata.core.edm.annotation.EdmIgnore;
+import com.sap.olingo.jpa.metadata.core.edm.annotation.EdmProtectedBy;
+
 import org.eclipse.persistence.annotations.Convert;
 import org.eclipse.persistence.annotations.TypeConverter;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import java.util.*;
 
 @Entity(name = "package")
 @Table(name = "packages")
@@ -69,4 +87,11 @@ public class PackageEntity {
 
     @OneToMany(mappedBy = "packageEntity", fetch = FetchType.LAZY)
     private Set<EventEntity> events;
+
+    @EdmProtectedBy(name = "tenant_id")
+    @EdmIgnore
+    @Column(name = "tenant_id", length = 256)
+    @Convert("uuidConverter")
+    @TypeConverter(name = "uuidConverter", dataType = Object.class, objectType = UUID.class)
+    private UUID tenant;
 }
