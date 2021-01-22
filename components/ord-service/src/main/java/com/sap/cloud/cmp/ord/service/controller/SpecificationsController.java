afeb8b5ec99ad99ec1d906758c64f293bb7abecb
Nikolay Stoykov <nikolay.stoykov@sap.com>
2021-01-22 12:13:31 +0200

    Check for tenant when getting specifications

--- components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/controller/SpecificationsController.java
+++ components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/controller/SpecificationsController.java
@@ -1,17 +1,24 @@
 package com.sap.cloud.cmp.ord.service.controller;
 
 
+import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.sap.cloud.cmp.ord.service.repository.ApiSpecRepository;
 import com.sap.cloud.cmp.ord.service.repository.EventSpecRepository;
+import com.sap.cloud.cmp.ord.service.storage.model.APISpecificationEntity;
+import com.sap.cloud.cmp.ord.service.storage.model.EventSpecificationEntity;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.UUID;
 
 @Controller
 public class SpecificationsController {
@@ -25,14 +32,17 @@ public class SpecificationsController {
 
     @RequestMapping(value = "/${odata.jpa.request_mapping_path}/api/{id}/specification", method = { RequestMethod.GET }, produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
-    public String getApiSpec(@PathVariable final String id) {
-        return apiSpecRepository.getOne(UUID.fromString(id)).getSpecData();
+    public String getApiSpec(HttpServletRequest request, HttpServletResponse response, @PathVariable final String id) {
+        String tenantHeader = request.getHeader("Tenant");
+        APISpecificationEntity apiSpec = apiSpecRepository.getByApiDefinitionIdAndTenant(UUID.fromString(id), UUID.fromString(tenantHeader));
+        return apiSpec.getSpecData();
     }
 
     @RequestMapping(value = "/${odata.jpa.request_mapping_path}/event/{id}/specification", method = { RequestMethod.GET }, produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
-    public String getEventSpec(@PathVariable final String id) {
-        return eventSpecRepository.getOne(UUID.fromString(id)).getSpecData();
+    public String getEventSpec(HttpServletRequest request, HttpServletResponse response, @PathVariable final String id) {
+        String tenantHeader = request.getHeader("Tenant");
+        EventSpecificationEntity eventSpec = eventSpecRepository.getByEventDefinitionIdAndTenant(UUID.fromString(id), UUID.fromString(tenantHeader));
+        return eventSpec.getSpecData();
     }
-
 }
--- components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/repository/ApiSpecRepository.java
+++ components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/repository/ApiSpecRepository.java
@@ -1,11 +1,14 @@
 package com.sap.cloud.cmp.ord.service.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import com.sap.cloud.cmp.ord.service.storage.model.APISpecificationEntity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface ApiSpecRepository extends JpaRepository<APISpecificationEntity, UUID> {
+    public APISpecificationEntity getByApiDefinitionIdAndTenant(UUID id, UUID tenant);
 }
