bcd6c7b779124b1fc6d70ce1d1d39ac2decd90cb
Angel Aleksandrov <angel.aleksandrov@sap.com>
2021-01-27 15:23:43 +0200

    delete leftover

--- components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/config/CusttomJPAEdmProvider.java
+++
@@ -1,6 +0,0 @@
-//package com.sap.cloud.cmp.ord.service.config;
-//import com.sap.olingo.jpa.metadata.api.JPAEdmProvider;
-//
-//public class CusttomJPAEdmProvider extends JPAEdmProvider{
-//	Custo
-//}
