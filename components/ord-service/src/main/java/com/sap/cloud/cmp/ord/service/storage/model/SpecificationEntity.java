8defac7586d7d3cdf4968e2ad8583d03f9d12d64
i356426 <dimitar.petrov@sap.com>
2021-02-04 16:04:34 +0200

    Allow multiple specifications per API/Event

--- components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/storage/model/SpecificationEntity.java
+++ components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/storage/model/SpecificationEntity.java
@@ -10,17 +10,22 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.UUID;
 
-/**
- * Once we introduce ORD Aggregator EventDefinitions should be fetched as Specifications.
- * Therefore Compass should support multiple specs per Event.
- * In order to achieve that we will create a new specifications table which will unify api/event specifications.
- */
-@Entity(name = "eventSpecification")
-@Table(name = "event_api_definitions")
+@Entity(name = "specification")
+@Table(name = "specifications")
 @EdmIgnore
-public class EventSpecificationEntity {
+public class SpecificationEntity {
     @javax.persistence.Id
     @Column(name = "id")
+    @Convert("uuidConverter")
+    @TypeConverter(name = "uuidConverter", dataType = Object.class, objectType = UUID.class)
+    private UUID specId;
+
+    @Column(name = "api_def_id")
+    @Convert("uuidConverter")
+    @TypeConverter(name = "uuidConverter", dataType = Object.class, objectType = UUID.class)
+    private UUID apiDefinitionId;
+
+    @Column(name = "event_def_id")
     @Convert("uuidConverter")
     @TypeConverter(name = "uuidConverter", dataType = Object.class, objectType = UUID.class)
     private UUID eventDefinitionId;
@@ -34,10 +39,6 @@ public class EventSpecificationEntity {
     @Convert("uuidConverter")
     @TypeConverter(name = "uuidConverter", dataType = Object.class, objectType = UUID.class)
     private UUID tenant;
-
-    public UUID getEventDefinitionId() {
-        return eventDefinitionId;
-    }
 
     public String getSpecData() {
         return specData;
