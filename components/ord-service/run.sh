f2365a9d81757c241e2ad45435fad2dbe5b04cdd
StanislavStefanov <stanislavsvetlinovstefanov@gmail.com>
2021-08-10 17:32:40 +0300

    change message

--- components/ord-service/run.sh
+++ components/ord-service/run.sh
@@ -39,7 +39,7 @@ done
 
 export SCHEMA_MIGRATION_VERSION=$(ls -lr $MIGRATIONS_PATH | head -n 2 | tail -n 1 | tr -s ' ' | cut -d ' ' -f9 | cut -d '_' -f1)
 
-echo "VERSIONN" $SCHEMA_MIGRATION_VERSION
+echo "Expected schema version" $SCHEMA_MIGRATION_VERSION
 
 if [[ ${SKIP_DEPS} = false ]]; then
     source "$COMPONENT_DIR/scripts/install_dependencies.sh"
