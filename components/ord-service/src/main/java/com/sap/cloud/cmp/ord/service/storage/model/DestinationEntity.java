af8adf9192bcfe097f9e895bbc57f6c322e53142
petar-iv <20043863+petar-iv@users.noreply.github.com>
2022-08-08 21:34:52 +0300

    Revert "Extract sensitive data as separate entity"
    This reverts commit 381459553a5178b8b69ba4ad28adc60b214b7f3d.

--- components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/storage/model/DestinationEntity.java
+++ components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/storage/model/DestinationEntity.java
@@ -1,5 +1,6 @@
 package com.sap.cloud.cmp.ord.service.storage.model;
 
+import com.sap.cloud.cmp.ord.service.storage.model.converter.SensitiveDataConverter;
 import com.sap.olingo.jpa.metadata.core.edm.annotation.EdmIgnore;
 import com.sap.olingo.jpa.metadata.core.edm.annotation.EdmProtectedBy;
 
@@ -9,8 +10,6 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 import org.eclipse.persistence.annotations.Convert;
@@ -31,7 +30,7 @@ public class DestinationEntity {
 
     @Column(name = "name", length = Integer.MAX_VALUE)
     private String name;
-
+    
     @Column(name = "type", length = Integer.MAX_VALUE)
     private String type;
 
@@ -43,9 +42,8 @@ public class DestinationEntity {
     @Column(name = "tenant_id", length = 256)
     private String tenant;
 
-    @OneToOne(mappedBy = "destination", fetch = FetchType.LAZY)
-    @PrimaryKeyJoinColumn
-    private DestinationSensitiveDataEntity sensitiveData;
+    @Column(name = "sensitive_data", length = Integer.MAX_VALUE)
+    private String sensitiveData;
 
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(
