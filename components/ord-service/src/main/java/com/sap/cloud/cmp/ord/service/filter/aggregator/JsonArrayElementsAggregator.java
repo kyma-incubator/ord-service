92abf31e993cdb3cf8260c2f1378472868bf2748
ivanetenevvasilev <ivantenevvasilev@abv.bg>
2023-08-09 11:31:55 +0300

    make embeddable logic generic

--- components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/filter/aggregator/JsonArrayElementsAggregator.java
+++ components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/filter/aggregator/JsonArrayElementsAggregator.java
@@ -11,6 +11,7 @@ public class JsonArrayElementsAggregator {
     private static final String[] subresources = new String[]{"value" /*This is the key returned when you list objects*/, "products", "packages", "consumptionBundles", "apis", "events", "vendors", "systemInstances"};
     private static final String[] arrays = new String[]{"tags", "countries", "lineOfBusiness", "industry", "entryPoints", "successors", "correlationIds", "partners"};
     private static final String[] labels = new String[]{"labels", "documentationLabels"};
+    private static final String[] embeddedResources = new String[]{"extensible"};
 
     private ObjectMapper mapper;
 
@@ -62,19 +63,22 @@ public class JsonArrayElementsAggregator {
                 ((ObjectNode) resource).replace(labelResource, convertedLabels);
             }
         }
-        JsonNode extensible = resource.get("extensible");
-        if (extensible != null && extensible.isObject()) {
-            Boolean isEmpty = true;
-            Iterator<JsonNode> it = extensible.elements();
-            while (it.hasNext()) {
-                JsonNode el = it.next();
-                if (!el.isNull()) {
-                    isEmpty = false;
-                    break;
+
+        for (String embeddedResource : embeddedResources) {
+            JsonNode extensible = resource.get(embeddedResource);
+            if (extensible != null && extensible.isObject()) {
+                Boolean isEmpty = true;
+                Iterator<JsonNode> it = extensible.elements();
+                while (it.hasNext()) {
+                    JsonNode el = it.next();
+                    if (!el.isNull()) {
+                        isEmpty = false;
+                        break;
+                    }
                 }
-            }
-            if (isEmpty) {
-                ((ObjectNode) resource).replace("extensible", null);
+                if (isEmpty) {
+                    ((ObjectNode) resource).replace(embeddedResource, null);
+                }
             }
         }
     }
