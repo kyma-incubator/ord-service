c0d96a3d853217c5486b8460b9fc43243906f90a
Nikolay Yordanov <n.yordanov@sap.com>
2023-12-13 19:19:25 +0200

    Adapt AspectEventResource entity (#114)
    * Adapt aspectEventResource entity
    
    * expose apis and events directly
    
    * Revert "expose apis and events directly"
    
    This reverts commit 34c6884c2317a54fc3c556b58430cba65255aae9.
    
    ---------
    
    Co-authored-by: ivanetenevvasilev <ivantenevvasilev@abv.bg>

--- components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/storage/model/AspectEventResource.java
+++ components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/storage/model/AspectEventResource.java
@@ -1,6 +1,7 @@
 package com.sap.cloud.cmp.ord.service.storage.model;
 
 import com.sap.olingo.jpa.metadata.core.edm.annotation.EdmIgnore;
+import com.sap.olingo.jpa.metadata.core.edm.annotation.EdmProtectedBy;
 import org.eclipse.persistence.annotations.Convert;
 import org.eclipse.persistence.annotations.TypeConverter;
 
@@ -9,14 +10,27 @@ import java.util.List;
 import java.util.UUID;
 
 @Entity(name = "aspectEventResource")
-@Table(name = "aspect_event_resources")
+@Table(name = "tenants_aspect_event_resources")
 public class AspectEventResource {
-    // omit @EdmIgnore annotation so that the expanding between `aspects` and `aspectEventResource` can work (in both directions)
     @javax.persistence.Id
-    @Column(name = "event_resource_id", length = 256)
+    @Column(name = "id")
     @Convert("uuidConverter")
     @TypeConverter(name = "uuidConverter", dataType = Object.class, objectType = UUID.class)
-    private UUID eventId;
+    private UUID Id;
+
+    @EdmProtectedBy(name = "tenant_id")
+    @EdmIgnore
+    @Column(name = "tenant_id", length = 256)
+    @Convert("uuidConverter")
+    @TypeConverter(name = "uuidConverter", dataType = Object.class, objectType = UUID.class)
+    private UUID tenant;
+
+    @EdmProtectedBy(name = "formation_scope")
+    @EdmIgnore
+    @Column(name = "formation_id")
+    @Convert("uuidConverter")
+    @TypeConverter(name = "uuidConverter", dataType = Object.class, objectType = UUID.class)
+    private UUID formationID;
 
     @Column(name = "ord_id", length = Integer.MAX_VALUE)
     private String ordId;
