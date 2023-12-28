b59c992a4cdea5bae9b9221671b60cdf5e23a121
DimitarPetrov <dimitar.petrov@sap.com>
2023-12-28 19:16:37 +0200

    Fix jsonb operator not working in spring jpa

--- components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/repository/SelfRegisteredRepository.java
+++ components/ord-service/src/main/java/com/sap/cloud/cmp/ord/service/repository/SelfRegisteredRepository.java
@@ -18,7 +18,7 @@ public interface SelfRegisteredRepository extends JpaRepository<RootEntity, Inte
     @Query(nativeQuery = true, value = "SELECT id FROM tenant_runtime_contexts trc WHERE trc.tenant_id = uuid(?1) AND trc.id IN (SELECT id FROM runtime_contexts WHERE runtime_id = uuid(?2)) AND trc.owner = true")
     String getRuntimeSubscriptionAvailableInTenant(String consumerTenantId, String runtimeId);
 
-    @Query(nativeQuery = true, value = "SELECT f.id FROM formations f WHERE (SELECT l.value FROM labels l WHERE l.key = 'scenarios' and l.runtime_context_id = uuid(?1)) ? f.name AND f.formation_template_id IN (SELECT ft.id FROM formation_templates ft WHERE ft.discovery_consumers ? (SELECT l.value->>0 FROM labels l WHERE l.key = 'runtimeType' AND l.runtime_id = (SELECT rtctx.runtime_id FROM runtime_contexts rtctx where rtctx.id = uuid(?1))))")
+    @Query(nativeQuery = true, value = "SELECT f.id FROM formations f WHERE jsonb_exists((SELECT l.value FROM labels l WHERE l.key = 'scenarios' and l.runtime_context_id = uuid(?1)), f.name) AND f.formation_template_id IN (SELECT ft.id FROM formation_templates ft WHERE jsonb_exists(ft.discovery_consumers, (SELECT l.value->>0 FROM labels l WHERE l.key = 'runtimeType' AND l.runtime_id = (SELECT rtctx.runtime_id FROM runtime_contexts rtctx where rtctx.id = uuid(?1)))))")
     Set<String> getFormationsThatRuntimeSubscriptionAvailableInTenantIsPartOf(String rtCtxID);
 
     @Query(nativeQuery = true, value = "SELECT l1.app_template_id FROM labels l1 JOIN labels l2 ON l1.app_template_id = l2.app_template_id JOIN labels l3 ON l2.app_template_id = l3.app_template_id WHERE l1.app_template_id IS NOT NULL AND l1.key = ?2 AND l1.value = to_jsonb(?3) AND l2.key = ?4 AND l2.value = to_jsonb(?5) AND l3.key = 'global_subaccount_id' AND l3.value IN (SELECT to_jsonb(external_tenant) FROM business_tenant_mappings WHERE id = uuid(?1))")
@@ -27,6 +27,6 @@ public interface SelfRegisteredRepository extends JpaRepository<RootEntity, Inte
     @Query(nativeQuery = true, value = "SELECT id FROM tenant_applications ta WHERE ta.tenant_id = uuid(?1) AND ta.id IN (SELECT id FROM applications WHERE app_template_id = uuid(?2)) AND ta.owner = true")
     String getApplicationSubscriptionAvailableInTenant(String consumerTenantId, String appTmplId);
 
-    @Query(nativeQuery = true, value = "SELECT f.id FROM formations f WHERE (SELECT l.value FROM labels l WHERE l.key = 'scenarios' and l.app_id = uuid(?1)) ? f.name AND f.formation_template_id IN (SELECT ft.id FROM formation_templates ft WHERE ft.discovery_consumers ? (SELECT l.value->>0 FROM labels l WHERE l.key = 'applicationType' AND l.app_id = uuid(?1)))")
+    @Query(nativeQuery = true, value = "SELECT f.id FROM formations f WHERE jsonb_exists((SELECT l.value FROM labels l WHERE l.key = 'scenarios' and l.app_id = uuid(?1)), f.name) AND f.formation_template_id IN (SELECT ft.id FROM formation_templates ft WHERE jsonb_exists(ft.discovery_consumers, (SELECT l.value->>0 FROM labels l WHERE l.key = 'applicationType' AND l.app_id = uuid(?1))))")
     Set<String> getFormationsThatApplicationSubscriptionAvailableInTenantIsPartOf(String appId);
 }
